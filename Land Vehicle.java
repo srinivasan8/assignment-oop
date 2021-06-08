b48e69c0aa918c0760588ad0cf3015bb51c238e0
srinivasan8 <72722578+srinivasan8@users.noreply.github.com>
2021-06-08 13:49:54 +0700

    Create Land Vehicle.java

---
+++ Land Vehicle.java
@@ -0,0 +1,11 @@
+  public void drive() {
+        return drive;
+    }
+   public void setNumberWheels() {
+         setNumberWheels=int(0,10)
+        return setNumberWheels;
+    }
+    
+     public void getNumberWheels() {
+        return getNumberWheels;
+    }
